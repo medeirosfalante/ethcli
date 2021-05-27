5189e62dff68eea4787c9250374402b704d55718
Rafael Medeiros <rafaelgurudesign@gmail.com>
2021-05-27 13:15:34 -0300

    change structure client ref

--- token_test.go
+++ token_test.go
@@ -1,8 +1,9 @@
-package ethecli_test
+package ethcli_test
 
 import (
 	"testing"
 
+	"github.com/ethereum/go-ethereum/ethclient"
 	ethcli "github.com/medeirosfalante/ethcli"
 
 	"github.com/joho/godotenv"
@@ -10,13 +11,20 @@ import (
 
 func TestSendTokenErc20(t *testing.T) {
 	godotenv.Load()
-	token := ethcli.NewTokenErc20("0xf35d75E2Ce765fD4aB1Da7b331eB03C56D4859c4", "https://data-seed-prebsc-2-s2.binance.org:8545")
+	client, err := ethclient.Dial("https://data-seed-prebsc-2-s2.binance.org:8545")
+	if err != nil {
+		t.Errorf("err : %s", err)
+		return
+	}
+	token := ethcli.NewTokenErc20("0xf35d75E2Ce765fD4aB1Da7b331eB03C56D4859c4", client)
 
 	tx, err := token.Transfer("0xB8A688D5A29a35B01CC00d0e2144E01d3c96bFC3", 350.50)
 	if err != nil {
 		t.Errorf("err : %s", err)
 		return
 	}
+
+	t.Errorf("tx is empany")
 
 	if tx == "" {
 		t.Errorf("tx is empany")
